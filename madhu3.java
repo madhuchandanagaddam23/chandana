62469d1b7573ad0329767da2b388e0f7c8ea3e0a
madhuchandanagaddam23 <madhuchandanagaddam23@gmail.com>
2025-08-16 10:37:47 +0530

    Add files via upload

---
+++ madhu3.java
@@ -0,0 +1,17 @@
+ Bitwise Operator Puzzle 
+
+What will this print? 
+
+int x = 5;   // 0101   
+
+int y = 3;   // 0011 
+
+System.out.println(x & y); 
+
+System.out.println(x | y); 
+
+System.out.println(x ^ y); 
+o/p:
+1
+7
+6
