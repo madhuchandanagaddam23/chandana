d07860c869ed9fbc65eadfaf961a552879340bb4
madhuchandanagaddam23 <madhuchandanagaddam23@gmail.com>
2025-08-16 10:39:19 +0530

    Add files via upload

---
+++ madhu6.java
@@ -0,0 +1,5 @@
+
+6)  What is JIT Compiler and how does it improve performance?
+JIT (Just-In-Time) Compiler is a part of the JVM’s Execution Engine.
+Instead of interpreting bytecode line by line, JIT compiles frequently used bytecode into native machine code (CPU instructions) at runtime.
+Once compiled, the machine code can be executed directly by the processor — much faster than interpretation.
