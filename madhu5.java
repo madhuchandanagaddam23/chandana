028fb5387fb926b2fda48bd6f770811001aad05e
madhuchandanagaddam23 <madhuchandanagaddam23@gmail.com>
2025-08-16 10:38:58 +0530

    Add files via upload

---
+++ madhu5.java
@@ -0,0 +1,3 @@
+Explain JVM architecture
+The Java Virtual Machine (JVM) is the runtime environment in which Java bytecode is executed.
+It acts like a virtual computer that runs Java programs independent of the operating system and hardware.
